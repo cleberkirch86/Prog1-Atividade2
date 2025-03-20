3ca2c76a02300492a15a82ce37e8cc9457742a9b
clebe <cleberkirch@gmail.com>
2025-03-20 20:25:30 -0300

    Criado classe Main com Maven e JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.atividade2_rev02 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.atividade2_rev02 to javafx.fxml;
+    exports org.example.atividade2_rev02;
+}
